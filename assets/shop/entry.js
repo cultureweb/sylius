60bbf30c273f2888159f6c1c44fedb8fee011171
cultureweb <cedric.milius@yahoo.fr>
2023-03-11 20:20:58 +0100

    Override header template and install bootstrap 5

--- assets/shop/entry.js
+++ assets/shop/entry.js
@@ -1,1 +1,8 @@
 // In this file you can import assets like images or stylesheets
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/style.css';
+
+// Get js
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './js/main.js';
